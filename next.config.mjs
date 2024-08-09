61fd644fdd4c87282fa0ecbbc15995af3a5a2030
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-09 11:38:34 +0300

    feat: add external image hostname to next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    httpAgentOptions:{
+        keepAlive: false,
+    },
+    images: {
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname:  "cdn.dummyjson.com",
+            }
+        ]
+    },
+};
 
 export default nextConfig;
