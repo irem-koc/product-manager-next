cb9d215b8ac2d724e25a4729f53a5892fc5db1bd
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-11 23:41:15 +0300

    feat: add input filter area to navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="flex justify-between items-center bg-slate-200 z-auto shadow-lg p-4 w-full h-50">
+    <div className="flex justify-around items-center bg-slate-200 z-auto shadow-lg p-4 w-full h-50">
       <div className="flex items-center gap-2">
         <Image
           alt="logo"
@@ -15,9 +15,16 @@ const Navbar = (props: Props) => {
           height={60}
           className="object-cover"
         />
-        <Link href="/" className="font-title text-4xl">
+        <Link href="/" className="font-title text-2xl">
           Product Manager App
         </Link>
+      </div>
+      <div className="w-1/3">
+        <input
+          className="w-full border p-3 rounded-md text-sm outline-none border-gray-200"
+          type="text"
+          placeholder="Aradığınız ürün, kategori veya markayı yazınız"
+        />
       </div>
       <div>
         <Links />
