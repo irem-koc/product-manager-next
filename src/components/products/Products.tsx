92bb5d79c835a9af16153c3f5281cc3ba90f3f01
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-11 23:39:40 +0300

    feat: add products result page

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,12 +1,23 @@
 "use client";
 import { useGetAllProducts } from "@/hooks/useGetAllProducts";
+import { Product } from "@/types/types";
+import ProducListItem from "../producListItem/ProducListItem";
 type Props = {};
 
 const Products = (props: Props) => {
   const { data, isLoading } = useGetAllProducts();
   console.log(data?.products?.length);
+  console.log(data, " --->>>> data");
   if (isLoading) return <div>Loading</div>;
-  return <div>Products length ---- {data?.products?.length}</div>;
+  return (
+    <div className="flex flex-wrap justify-center mx-auto my-5">
+      {data?.products &&
+        !isLoading &&
+        data?.products?.map((product: Product) => (
+          <ProducListItem key={product.id} product={product} />
+        ))}
+    </div>
+  );
 };
 
 export default Products;
