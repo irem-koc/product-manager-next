a349128b37a09db3b483ed5d7b689dc09d9780af
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-11 23:38:41 +0300

    feat: add filter section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import LeftMenu from "@/components/leftMenu/LeftMenu";
 import Products from "@/components/products/Products";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex mx-20 overflow-x-auto">
+      <LeftMenu />
       <Products />
-    </>
+    </main>
   );
 }
