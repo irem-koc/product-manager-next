be0ea07276de6df27123f8c4317240125defe6fd
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-13 22:22:15 +0300

    feat: add get product by id query

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,10 @@
-const ProductIemDetail = () => {
-  return <div>ProductIemDetail</div>;
+"use client";
+import { useGetProduct } from "@/hooks/useGetProduct";
+
+const ProductItemDetail = ({ params }) => {
+  const { id } = params;
+  const { data, isLoading } = useGetProduct(id);
+  return <div>ProductItemDetail</div>;
 };
 
-export default ProductIemDetail;
+export default ProductItemDetail;
