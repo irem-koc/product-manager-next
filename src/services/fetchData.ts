e4564f6d429a8cbb3a9f6b52dc58a856a255305a
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-13 22:22:40 +0300

    feat: add getProduct request

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,7 +1,7 @@
 export const getAllProducts = async () => {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API}/products?limit=0`);
-    
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -11,3 +11,15 @@ export const getAllProducts = async () => {
     throw new Error(`Error while fetching products: ${error}`);
   }
 };
+export const getProduct = async (id:string) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/products/${id}`);
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Error while fetching product: ${error}`);
+  }
+}
